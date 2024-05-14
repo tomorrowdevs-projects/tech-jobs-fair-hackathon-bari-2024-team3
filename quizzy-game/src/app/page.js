005b173c675913c6f1e4e8cb04b0908903959426
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-14 22:32:40 +0200

    reindent e divisione css

--- quizzy-game/src/app/page.js
+++ quizzy-game/src/app/page.js
@@ -5,65 +5,24 @@ import Link from 'next/link';
 export default function Home() {
   
   return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      justifyContent="flex-start"
-      alignItems="center"
-      height="100vh"
-    >
-      <Image
-        src="/image-logo.svg"
-        alt="Descrizione dell'immagine"
-        width="256px"
-        height="256px"
-        marginTop="180px" 
+    <Box className="box">
 
-      />
+      <Image src="/image-logo.svg" alt="Descrizione dell'immagine" className="image"/>
       
       <Flex alignItems="center" flexDirection="column" marginTop="190px">
         <InputGroup>
-          <InputLeftElement pointerEvents='none'>
-          </InputLeftElement>
-          <Input type='rom' placeholder='Room Code' width="315px" height="48px" border="2px"/>
+          <InputLeftElement pointerEvents='none'></InputLeftElement>
+          <Input type='rom' placeholder='Room Code' className='input-room-code'/>
         </InputGroup>
         
         <Link href="#" passHref>
-          <Button
-            as="a"
-            size="lg"
-            height="48px"
-            width="320px"
-            border="2px"
-            borderColor="green.500"
-            marginTop="20px"
-            bg="#ECC94B"
-            style={{ pointerEvents: 'none' }}
-            color="black"
-          >
-            Enter
-          </Button>
+          <Button as="a" className='button-one'>Enter</Button>
         </Link>
+
         <Link href="#" passHref>
-          <Button
-            as="a"
-            size="lg"
-            height="40px"
-            width="40px"
-            border="2px"
-            borderColor="green.500"
-            marginTop="40px"
-            bg="#ECC94B"
-            marginLeft="280px"
-            fontSize="24px"
-            lineHeight="40px" 
-            style={{ pointerEvents: 'none' }}
-            color="black"
-          >
-            +
-          </Button>
+          <Button as="a" className='button-two'>+</Button>
         </Link>
-        
+
       </Flex>
     </Box>
   );
