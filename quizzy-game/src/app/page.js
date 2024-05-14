8176624db259590b71c7797ca74ddbfa2d594d0c
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-14 16:27:35 +0200

    terminata la pagina home

--- quizzy-game/src/app/page.js
+++ quizzy-game/src/app/page.js
@@ -1,6 +1,9 @@
 import { Box, Image, Button, InputGroup, Input, InputLeftElement, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+
 
 export default function Home() {
+  
   return (
     <Box
       display="flex"
@@ -14,27 +17,49 @@ export default function Home() {
         alt="Descrizione dell'immagine"
         width="256px"
         height="256px"
-        marginTop="110px" 
-      />
+        marginTop="180px" 
 
-      <Flex alignItems="center" flexDirection="column" marginTop="100px">
-        <InputGroup width="320px" height="48px">
+      />
+      
+      <Flex alignItems="center" flexDirection="column" marginTop="190px">
+        <InputGroup>
           <InputLeftElement pointerEvents='none'>
           </InputLeftElement>
-          <Input type='rom' placeholder='Room Code' />
+          <Input type='rom' placeholder='Room Code' width="315px" height="48px" border="2px"/>
         </InputGroup>
-
-        <Button
-          size="lg"
-          height="48px"
-          width="320px"
-          border="2px"
-          borderColor="green.500"
-          marginTop="20px"
-          bg="#ECC94B"
-        >
-          Enter
-        </Button>
+        
+        <Link href="#" passHref>
+          <Button
+            as="a"
+            size="lg"
+            height="48px"
+            width="320px"
+            border="2px"
+            borderColor="green.500"
+            marginTop="20px"
+            bg="#ECC94B"
+          >
+            Enter
+          </Button>
+        </Link>
+        <Link href="#" passHref>
+          <Button
+            as="a"
+            size="lg"
+            height="40px"
+            width="40px"
+            border="2px"
+            borderColor="green.500"
+            marginTop="40px"
+            bg="#ECC94B"
+            marginLeft="280px"
+            fontSize="24px"
+            lineHeight="40px" 
+          >
+            +
+          </Button>
+        </Link>
+        
       </Flex>
     </Box>
   );
