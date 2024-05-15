4776ddb1786d6a97fda5af5ee9017d66e3a75c39
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-15 15:37:35 +0200

    cerazione dahboard new-game

--- quizzy-game/src/app/new-game/page.js
+++ quizzy-game/src/app/new-game/page.js
@@ -1,1 +1,27 @@
-'use client'
+
+'user client'
+import { Box, Image, Button, InputGroup, Input, InputLeftElement, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+import  './style/style-new-game.css';
+
+
+
+export default function Home() {
+  return (
+    <Box className="box">
+      <Image src="/image-logo.svg" alt="Descrizione dell'immagine" className="image"/>
+      <Flex alignItems="center" flexDirection="column" marginTop="190px">
+        <InputGroup>
+          <InputLeftElement pointerEvents='none'></InputLeftElement>
+          <Input type='rom' placeholder='1312lamwlkdaw' className='input-room-code'/>
+        </InputGroup>
+        <Link href="#">
+          <Button className='button-one'>Enter</Button>
+        </Link>
+        <Link href="#">
+          <Button className='button-two'>+</Button>
+        </Link>
+      </Flex>
+    </Box>
+  );
+}
