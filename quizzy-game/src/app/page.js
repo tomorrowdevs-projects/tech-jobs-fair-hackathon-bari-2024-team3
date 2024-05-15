0d4a7011746402866d42da7deec99fb442d48657
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-15 11:48:39 +0200

    .

--- quizzy-game/src/app/page.js
+++ quizzy-game/src/app/page.js
@@ -3,52 +3,20 @@ import { Box, Image, Button, InputGroup, Input, InputLeftElement, Flex } from '@
 import Link from 'next/link';
 
 export default function Home() {
-  
   return (
     <Box className="box">
-
       <Image src="/image-logo.svg" alt="Descrizione dell'immagine" className="image"/>
-      
       <Flex alignItems="center" flexDirection="column" marginTop="190px">
         <InputGroup>
           <InputLeftElement pointerEvents='none'></InputLeftElement>
           <Input type='rom' placeholder='Room Code' className='input-room-code'/>
         </InputGroup>
         <Link href="#">
-          <Button
-            as="a"
-            size="lg"
-            height="48px"
-            width="320px"
-            border="2px"
-            borderColor="green.500"
-            marginTop="20px"
-            bg="#ECC94B"
-            style={{ pointerEvents: 'none' }}
-            color="black"
-          >
-            Enter
-          </Button>
-          </Link>
-          <Link href="#">
-          <Button
-            as="a"
-            size="lg"
-            height="40px"
-            width="40px"
-            border="2px"
-            borderColor="green.500"
-            marginTop="40px"
-            bg="#ECC94B"
-            marginLeft="280px"
-            fontSize="24px"
-            lineHeight="40px" 
-            style={{ pointerEvents: 'none' }}
-            color="black"
-          > 
-            +
-          </Button>
-          </Link>
+          <Button className='button-one'>Enter</Button>
+        </Link>
+        <Link href="#">
+          <Button className='button-two'>+</Button>
+        </Link>
       </Flex>
     </Box>
   );
