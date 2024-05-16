53b07d1032ffa378db62ebda1290259f4c91505d
Triana Laurino <trii.laurino@gmail.com>
2024-05-16 12:28:44 +0200

    Added map method in new game waiting

--- quizzy-game/src/app/new-game-waiting/page.js
+++ quizzy-game/src/app/new-game-waiting/page.js
@@ -1,9 +1,42 @@
 'use client'
+import { Box, Image, Table, Thead, Tbody, Tr, Th, Td, TableContainer, Button} from '@chakra-ui/react';
+import './page-modules.css';
 
-export default function Home() {
+
+
+export default function NewGameWaiting({ players = ['Player 1', 'Player 2'] }) {
+    
     return (
-      <div>
-        this is gonna be the waiting room!!
-      </div>
+     
+      <Box className="box">
+        <Image src="/image-logo.svg" alt="Quizzy game logo" className="image"/>
+
+        <TableContainer className="tableContainer" >
+          <Table variant='simple' className="table">
+            <Thead>
+              <Tr>
+                <Th className="titleTable">PLAYER</Th>
+                <Th>AVATAR</Th>
+              </Tr>
+            </Thead>
+            {players.map((players, index) =>(
+              <Tbody key={index}>
+              <Tr>
+                <Td>{players}</Td>
+                <Td>pass the value</Td>
+              </Tr>
+            </Tbody>
+              ))}
+          </Table>
+        </TableContainer>
+        <Button
+          isLoading
+          colorScheme='yellow'
+        >
+          Waiting for game start...
+        </Button>
+
+      </Box>
+      
     );
   }
