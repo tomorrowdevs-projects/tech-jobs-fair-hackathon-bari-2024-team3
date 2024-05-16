0e0fe550a3eae82c4b14242828c4555858a7b74a
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-16 15:55:40 +0200

    risolto

--- quizzy-game/src/app/layout.js
+++ quizzy-game/src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       
-      <body className={inter.className}><ChakraProvider>{children}</ChakraProvider></body>
+      <body className={inter.className}>{children}</body>
       
     </html>
   );
