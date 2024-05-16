ad869273bd447796dd9e9a023160feffe616be1b
GabrieleGarofalo <124597681+GabrieleGarofalo@users.noreply.github.com>
2024-05-16 09:44:56 +0200

    creazione della dashbord answer con id statico ora devo integrare le API

--- quizzy-game/src/app/new-game/[id-game]/page.js
+++ quizzy-game/src/app/new-game/[id-game]/page.js
@@ -1,1 +1,28 @@
 'user client'
+import { Box, Image, Button, InputGroup, Input, InputLeftElement, Flex } from '@chakra-ui/react';
+import Link from 'next/link';
+import  './style/style-new-game-id.css';
+const GamePage = () => {
+    const idGame = 123; // Valore statico da rendere dinamico
+    return (
+        <div>
+            {/* <h1>Game ID: {idGame}</h1> */}
+            <Box className="box">
+                <Image src="/image-logo.svg" alt="Descrizione dell'immagine" className="image"/>
+                <Flex alignItems="center" flexDirection="column" marginTop="100px">
+                    <InputGroup>
+                    <InputLeftElement pointerEvents='none'></InputLeftElement>
+                    <Input type='rom' placeholder='1312lamwlkdaw' className='input-room-code'/>
+                    </InputGroup>
+                    <Button className='button-one'>Answer 1</Button>
+                    <Button className='button-one'>Answer 2</Button>
+                    <Button className='button-one'>Answer 3</Button>
+                    <Button className='button-one'>Answer 4</Button>
+                </Flex>
+            </Box>
+        </div>
+    );
+};
+
+export default GamePage;
+
