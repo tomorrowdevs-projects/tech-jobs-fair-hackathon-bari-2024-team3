d3ce4fefef706d35ff14a57e84eb07aab4caf4ee
Triana Laurino <trii.laurino@gmail.com>
2024-05-17 18:30:34 +0200

    updates in files winners

--- quizzy-game/src/app/winners/page.js
+++ quizzy-game/src/app/winners/page.js
@@ -3,29 +3,26 @@ import { Box, Image, Heading, Avatar, Button, Link } from '@chakra-ui/react';
 import './page-modules.css'; 
 
 
-export default function Winners({ winners }) { 
+export default function Winners({ winners = ['Player 1', 'Player 2', 'Player 3']}) { 
     return ( 
         <Box className="box"> 
         <Image src="/image-logo.svg" alt="Quizzy game logo" className="image" /> 
-            {winners.length > 0 && ( 
+            
             <div>
                 <Heading>WINNER!</Heading> 
                 <h3>{winners[0]}</h3> 
                 <Avatar className="winnerOne" /> 
-            </div> )} 
+            </div> 
             <div className="otherWinners"> 
-            {winners.length > 1 && ( 
-            <>
+            
             <Heading>2nd</Heading> 
-            <h3>{topThreeWinners[1]}</h3> 
+            <h3>{winners[1]}</h3> 
             <Avatar className="winnerTwo" /> 
-            </> )} 
-            {winners.length > 2 && ( 
-            <> 
+            
             <Heading>3rd</Heading> 
             <h3>{winners[2]}</h3> 
             <Avatar className="winnerThree" /> 
-            </> )} 
+            
             </div> 
             <Link href='#'> 
             <Button className="button">MAIN MENU</Button>
